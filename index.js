77e683cfe97a1fc93099b5958cf132ec8fb4ce7d
jddesign17 <jddesign234@gmail.com>
2024-11-25 02:04:02 +0530

    saved

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ app.get("/", (req, res) => {
     res.send("Hello from backend");
 });
 
+
+const data = [
+    {
+        id:1,
+        name: "John",
+        age: 30 
+    },
+    {
+        id:2,
+        name: "Jane",
+        age: 25 
+    },
+    {
+        id:3,
+        name: "Bob",
+        age: 40
+    }
+]
+
+app.get("/api", (req, res) => {    
+    res.json({message: data});
+});
+
 app.listen(3001, () => console.log("Server running on port 3001")); 
